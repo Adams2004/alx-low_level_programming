646a7a0ce0d1d76f7f8d87816913b80afde36ef4
Adams2004 <chrisadamsadewuni@gmail.com>
2022-09-26 15:29:27 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_memset - fills memory with a constant byte n amount of times
+ *
+ *@s: pointer that has the address of the memory to fill
+ *@b: constant byte that will be written into the momory
+ *@n: amount of times the byte will be written into the memory
+ *
+ *Return: address of the memory where thebyte was written
+ */
+int _putchar(char c)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i]" =" b;
+	}
+	return (s);
+'}'
